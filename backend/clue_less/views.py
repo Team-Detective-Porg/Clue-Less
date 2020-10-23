9d2201455cf2e71b3b003cb58d3fbc247aebbcf4
Bryce Beuerlein <swarvingprogrammer@gmail.com>
2020-10-23 00:27:24 -0400

    Add api serializer views.

--- backend/clue_less/views.py
+++ backend/clue_less/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CharacterSerializer, RoomSerializer, WeaponSerializer
+from .models import Character, Room, Weapon
 
-# Create your views here.
+
+class CharacterView(viewsets.ModelViewSet):
+    serializer_class = CharacterSerializer
+    queryset = Character.objects.all()
+
+
+class RoomView(viewsets.ModelViewSet):
+    serializer_class = RoomSerializer
+    queryset = Room.objects.all()
+
+
+class WeaponView(viewsets.ModelViewSet):
+    serializer_class = WeaponSerializer
+    queryset = Weapon.objects.all()
