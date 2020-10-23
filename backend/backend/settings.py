346a24b6f60cd25eb33341e5c54bc394e0d768ad
Bryce Beuerlein <swarvingprogrammer@gmail.com>
2020-10-23 00:24:37 -0400

    Add CORS and rest framework.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "clue_less",
+    "corsheaders",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMidleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -119,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+# TODO Adjust port as needed
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
