6654f5146b91f074179b175c710ede8f3b66b57b
Bryce Beuerlein <swarvingprogrammer@gmail.com>
2020-10-23 00:16:53 -0400

    Create admin interfaces.

--- backend/clue_less/admin.py
+++ backend/clue_less/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Character, Room, Weapon
 
-# Register your models here.
+
+# Create Character Admin interface
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ("holder", "location", "name")
+
+
+admin.site.register(Character, CharacterAdmin)
+
+
+# Create Room Admin interface
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("holder", "name")
+
+
+admin.site.register(Room, RoomAdmin)
+
+
+# Create Weapon Admin interface
+class WeaponAdmin(admin.ModelAdmin):
+    list_display = ("holder", "location", "name")
+
+
+admin.site.register(Weapon, WeaponAdmin)
