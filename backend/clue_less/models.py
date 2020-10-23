a71ff160d6b965ae46b7e4a330af62411765047e
Bryce Beuerlein <swarvingprogrammer@gmail.com>
2020-10-22 23:57:13 -0400

    Add Chracter model.

--- backend/clue_less/models.py
+++ backend/clue_less/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=120)
+    starting_place = models.CharField()  # TODO Better define this data type.
+    holder = models.ForeignKey("User", on_delete=models.SET_NULL, blank=True)
+
+    def _str_(self):
+        return self.name
