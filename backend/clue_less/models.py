f3eb5e66300acb796d5db7c47ba27737fc43eded
Bryce Beuerlein <swarvingprogrammer@gmail.com>
2020-10-23 00:16:24 -0400

    Fix missing argument and foreign key errors.

--- backend/clue_less/models.py
+++ backend/clue_less/models.py
@@ -6,9 +6,19 @@ from django.db import models
 class Character(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(
-        default="starting Place"
+        default="starting Place", max_length=10
     )  # TODO Better define this data type and default.
-    holder = models.ForeignKey("User", on_delete=models.SET_NULL, blank=True)
+    holder = models.ForeignKey(
+        "Player", on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    def _str_(self):
+        return self.name
+
+
+# TODO Replace this placeholder that is used to allow Foreign Keys to work.
+class Player(models.Model):
+    name = models.CharField(max_length=120)
 
     def _str_(self):
         return self.name
@@ -16,7 +26,9 @@ class Character(models.Model):
 
 class Room(models.Model):
     name = models.CharField(max_length=120)
-    holder = models.ForeignKey("User", on_delete=models.SET_NULL, blank=True)
+    holder = models.ForeignKey(
+        "Player", on_delete=models.SET_NULL, blank=True, null=True
+    )
 
     def _str_(self):
         return self.name
@@ -25,9 +37,11 @@ class Room(models.Model):
 class Weapon(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(
-        default="Starting Place"
+        default="Starting Place", max_length=10
     )  # TODO Better define this data type and default.
-    holder = models.ForeignKey("User", on_delete=models.SET_NULL, blank=True)
+    holder = models.ForeignKey(
+        "Player", on_delete=models.SET_NULL, blank=True, null=True
+    )
 
     def _str_(self):
         return self.name
