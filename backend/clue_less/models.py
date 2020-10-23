e3966cbc3036897129a718c85b04ec7597ed9fba
Bryce Beuerlein <swarvingprogrammer@gmail.com>
2020-10-23 00:01:43 -0400

    Add Weapon model.

--- backend/clue_less/models.py
+++ backend/clue_less/models.py
@@ -5,7 +5,9 @@ from django.db import models
 
 class Character(models.Model):
     name = models.CharField(max_length=120)
-    starting_place = models.CharField()  # TODO Better define this data type.
+    location = models.CharField(
+        default="starting Place"
+    )  # TODO Better define this data type and default.
     holder = models.ForeignKey("User", on_delete=models.SET_NULL, blank=True)
 
     def _str_(self):
@@ -14,6 +16,17 @@ class Character(models.Model):
 
 class Room(models.Model):
     name = models.CharField(max_length=120)
+    holder = models.ForeignKey("User", on_delete=models.SET_NULL, blank=True)
+
+    def _str_(self):
+        return self.name
+
+
+class Weapon(models.Model):
+    name = models.CharField(max_length=120)
+    location = models.CharField(
+        default="Starting Place"
+    )  # TODO Better define this data type and default.
     holder = models.ForeignKey("User", on_delete=models.SET_NULL, blank=True)
 
     def _str_(self):
