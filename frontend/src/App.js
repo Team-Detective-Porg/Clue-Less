01952a941d1315514118ad66ceb8e24f098f1c8e
Shirley Cheung <s.cheung44@gmail.com>
2020-10-25 22:19:44 -0500

    skeleton of multi-page application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Homepage from "./components/Homepage";
-
+import Main from "./components/Main"
 
 export default function App() {
 
     return (
-        <Homepage/>
+        <Main/>
     );
 }
