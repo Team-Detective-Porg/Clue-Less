28e503cda34b99fa3e5991412da47f3e53dea638
Shirley Cheung <s.cheung44@gmail.com>
2020-11-12 01:47:10 -0600

    file directory clean up

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Study from '../components/Study.js';
-import Hall from '../components/Hall.js';
-import Lounge from '../components/Lounge.js';
-import Library from '../components/Library.js';
-import Billiard from '../components/Billiard.js';
-import Dining from '../components/Dining.js';
-import Conservatory from '../components/Conservatory.js';
-import Ballroom from '../components/Ballroom.js';
-import Kitchen from '../components/Kitchen.js';
-import HorizontalHallway from '../components/HorizontalHallway.js';
-import VerticalHallway from '../components/VerticalHallway.js';
+import Study from '../rooms/Study.js';
+import Hall from '../rooms/Hall.js';
+import Lounge from '../rooms/Lounge.js';
+import Library from '../rooms/Library.js';
+import Billiard from '../rooms/Billiard.js';
+import Dining from '../rooms/Dining.js';
+import Conservatory from '../rooms/Conservatory.js';
+import Ballroom from '../rooms/Ballroom.js';
+import Kitchen from '../rooms/Kitchen.js';
+import HorizontalHallway from '../hallways/HorizontalHallway.js';
+import VerticalHallway from '../hallways/VerticalHallway.js';
 import Grid from '@material-ui/core/Grid';
-import Empty from '../components/Empty.js';
+import Empty from '../rooms/Empty.js';
 
 
 export default function Game(props) {
