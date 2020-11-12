27f6412eb0fc9b9625e2fa3b9672a3f9b7b360a6
Shirley Cheung <s.cheung44@gmail.com>
2020-11-12 01:10:28 -0600

    vertical alignment

---
+++ frontend/src/components/VerticalHallway.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: "0px"
+    }
+});
+
+export default function HorizontalHallway(props) {
+    const styling = useStyles();
+
+    const handleClick = () => {
+        console.log("Clicked hallway");
+    }
+
+    return (
+        <Grid item>
+            <div style={{width: "125px", height: "125px"}}>
+                <Grid container justify="center">
+                    <Grid item>
+                        <Button variant="contained" style={{width:"50px", height: "125px"}} onClick={handleClick}>
+                        </Button>
+                    </Grid>
+                </Grid>
+            </div>
+        </Grid>
+        
+    );
+}
--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -1,23 +1,61 @@
 import React from 'react';
 import Study from '../components/Study.js';
+import Hall from '../components/Hall.js';
+import Lounge from '../components/Lounge.js';
+import HorizontalHallway from '../components/HorizontalHallway.js';
+import VerticalHallway from '../components/VerticalHallway.js';
 import Grid from '@material-ui/core/Grid';
+import Empty from '../components/Empty.js';
 
 export default function Game(props) {
     return (
-        <Grid container direction="column" alignItems="center">
-            <Grid item>
-                <Study/>
+        <Grid container direction="row" alignItems="center">
+            {/* Row 1 */}
+            <Grid item xs={12}>
+                <Grid container justify="center" alignItems="center">
+                    <Study/>
+                    <HorizontalHallway/>
+                    <Hall/>
+                    <HorizontalHallway/>
+                    <Lounge/>
+                </Grid>
+            </Grid>
+            
+            <Grid item xs={12}>
+                <Grid container justify="center" alignItems="center">
+                    <Grid item>
+                        <VerticalHallway/>
+                    </Grid>
+                    
+                    <Grid item>
+                        <Empty/>
+                    </Grid>
+                    
+
+                    <Grid item>
+                        <VerticalHallway/>
+                    </Grid>
+                    
+                    <Grid item>
+                        <Empty/>
+                    </Grid>
+
+                    <Grid item>
+                        <VerticalHallway/>
+                    </Grid>
+                </Grid>
             </Grid>
 
-            <Grid item>
-
-            </Grid>
-
-            <Grid item>
-
+            <Grid item xs={12}>
+                <Grid container justify="center" alignItems="center">
+                    <Study/>
+                    <HorizontalHallway/>
+                    <Hall/>
+                    <HorizontalHallway/>
+                    <Lounge/>
+                </Grid>
             </Grid>
         </Grid>
-
-        
+  
     );
 }
