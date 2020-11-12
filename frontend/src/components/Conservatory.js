5081a5866684ea16ba60da8c2cb027ffbdc3d387
Shirley Cheung <s.cheung44@gmail.com>
2020-11-12 01:25:15 -0600

    initial gameboard set up

--- frontend/src/components/Conservatory.js
+++ frontend/src/components/Conservatory.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles({
 });
 
 
-export default function Study(props) {
+export default function Conservatory(props) {
     const styling = useStyles();
 
     const handleRoomClick = () => {
@@ -25,20 +25,16 @@ export default function Study(props) {
         <Grid item>
             <Paper variant="outlined" style={{width: "125px", height: "125px"}} >
                 <Button variant="contained" color="primary" style={{width:"100%", height: "100%", zIndex:10}} onClick={handleRoomClick}>
-                    <Grid container direction="column">
+                    <Grid container direction="column" alignItems="center">
                         <Grid item>
-                            Study
+                            <span style={{fontSize: "8pt"}}>
+                                Conservatory
+                            </span>
                         </Grid>
 
                         <Grid item>
                             <Paper>
-                                Shirley
-                            </Paper>
-                        </Grid>
-
-                        <Grid item>
-                            <Paper>
-                                Halle
+                                
                             </Paper>
                         </Grid>
                     </Grid>
@@ -52,7 +48,7 @@ export default function Study(props) {
                     style={{zIndex:20, padding:"0px", width: "25px", height: "25px", marginTop: "-60px", marginLeft: "95px"}} 
                     onClick={handlePassageClick}
                 >
-                    &#8600;
+                    &#8599;
                 </Button>
 
             </Paper>
--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Study from '../components/Study.js';
 import Hall from '../components/Hall.js';
 import Lounge from '../components/Lounge.js';
+import Library from '../components/Library.js';
+import Billiard from '../components/Billiard.js';
+import Dining from '../components/Dining.js';
+import Conservatory from '../components/Conservatory.js';
+import Ballroom from '../components/Ballroom.js';
+import Kitchen from '../components/Kitchen.js';
 import HorizontalHallway from '../components/HorizontalHallway.js';
 import VerticalHallway from '../components/VerticalHallway.js';
 import Grid from '@material-ui/core/Grid';
 import Empty from '../components/Empty.js';
 
+
 export default function Game(props) {
     return (
         <Grid container direction="row" alignItems="center">
-            {/* Row 1 */}
             <Grid item xs={12}>
                 <Grid container justify="center" alignItems="center">
                     <Study/>
@@ -23,38 +29,45 @@ export default function Game(props) {
             
             <Grid item xs={12}>
                 <Grid container justify="center" alignItems="center">
-                    <Grid item>
-                        <VerticalHallway/>
-                    </Grid>
-                    
-                    <Grid item>
-                        <Empty/>
-                    </Grid>
-                    
-
-                    <Grid item>
-                        <VerticalHallway/>
-                    </Grid>
-                    
-                    <Grid item>
-                        <Empty/>
-                    </Grid>
-
-                    <Grid item>
-                        <VerticalHallway/>
-                    </Grid>
+                    <VerticalHallway/>
+                    <Empty/>
+                    <VerticalHallway/>
+                    <Empty/>
+                    <VerticalHallway/>
                 </Grid>
             </Grid>
 
             <Grid item xs={12}>
                 <Grid container justify="center" alignItems="center">
-                    <Study/>
+                    <Library/>
                     <HorizontalHallway/>
-                    <Hall/>
+                    <Billiard/>
                     <HorizontalHallway/>
-                    <Lounge/>
+                    <Dining/>
                 </Grid>
             </Grid>
+
+            <Grid item xs={12}>
+                <Grid container justify="center" alignItems="center">
+                    <VerticalHallway/>
+                    <Empty/>
+                    <VerticalHallway/>
+                    <Empty/>
+                    <VerticalHallway/>
+                </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Grid container justify="center" alignItems="center">
+                    <Conservatory/>
+                    <HorizontalHallway/>
+                    <Ballroom/>
+                    <HorizontalHallway/>
+                    <Kitchen/>
+                </Grid>
+            </Grid>
+
+
         </Grid>
   
     );
