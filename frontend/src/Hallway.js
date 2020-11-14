0f45926ade1142b48491a020d03fd5ca173a3e5e
Shirley Cheung <s.cheung44@gmail.com>
2020-11-13 22:04:47 -0600

    simplified to room and hallway components

---
+++ frontend/src/Hallway.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: "0px"
+    }
+});
+
+export default function Hallway(props) {
+    const styling = useStyles();
+
+    return (
+        <Grid item style={{width: "125px", height: "125px"}}>
+            {
+                props.direction === "horizontal" ?
+                <Grid container alignItems="center" style={{width: "125px", height: "125px"}}>
+                    <Grid item>
+                        <Button 
+                            variant="contained" 
+                            style={{width:"125px", height: "50px"}} 
+                            onClick={() => {props.handleClick(props.hallwayType)}}/>
+                    </Grid>
+                </Grid>
+                : 
+                <Grid container justify="center">
+                    <Grid item>
+                        <Button 
+                            variant="contained" 
+                            style={{width:"50px", height: "125px"}} 
+                            onClick={() => {props.handleClick(props.hallwayType)}}/>
+                    </Grid>
+                </Grid>
+            }
+        </Grid>
+    );
+}
