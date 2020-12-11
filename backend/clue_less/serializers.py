a3a6756a03727beb21f242f57aecb282afafbe14
Bryce Beuerlein <swarvingprogrammer@gmail.com>
2020-12-10 21:15:48 -0500

    Add `id` to PlayerSerializer return.

--- backend/clue_less/serializers.py
+++ backend/clue_less/serializers.py
@@ -49,6 +49,7 @@ class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = (
+            "id",
             "active",
             "characterList",
             "game_session",
