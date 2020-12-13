f9cf9074f1d2d3d386b04677c32c3e453df6ca4e
Shirley Cheung <s.cheung44@gmail.com>
2020-12-13 12:50:48 -0600

    push history.js

---
+++ frontend/src/History.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+
+export default function History(props) {
+
+    return (
+        <Paper variant="outlined">
+            <Grid container alignItems="flex-start">
+                {props.history.map((item, index) => <Grid item xs={12} key={index}>{item}</Grid>)}
+            </Grid>
+        </Paper>  
+    );
+}
